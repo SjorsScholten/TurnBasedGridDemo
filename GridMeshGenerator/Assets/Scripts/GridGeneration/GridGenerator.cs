7114953a25f1ae8cdf82bb418c5d78787fcc1f9e
Sjors Scholten <scholtensjors22@gmail.com>
2020-01-07 22:15:11 +0100

    ignore idea and placing units

---
+++ GridMeshGenerator/Assets/Scripts/GridGeneration/GridGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridGenerator : MonoBehaviour {
+    public int width, length;
+    public Transform player;
+
+    public GridMap GridMap { get; private set; }
+
+    private void Start() {
+        VertexMap vertexMap = VertexMap.GenerateVertexMap(width, length);
+        GridMap = GridMap.GenerateGrid(vertexMap);
+        GenerateMesh();
+    }
+
+    public void GenerateMesh() {
+        MeshData meshData = MeshData.GenerateMeshData(GridMap);
+        Mesh mesh = new Mesh{vertices = GridMap.VertexMap.Vertices, triangles = meshData.Triangles};
+        mesh.RecalculateNormals();
+        GetComponent<MeshFilter>().sharedMesh = mesh;
+        GetComponent<MeshCollider>().sharedMesh = mesh;
+    }
+
+    private void OnDrawGizmos() { //mostly debug functions
+        if (GridMap == null) return;
+        
+        //debug vertices
+        Gizmos.color = Color.black;
+        foreach (var vertex in GridMap.VertexMap.Vertices) { Gizmos.DrawSphere(vertex, 0.1f); }
+        
+        //debug grid
+        foreach (Cell cell in GridMap.Cells) {
+            Gizmos.color = (cell.Walkable) ? Color.green : Color.red;
+            Gizmos.DrawWireCube(cell.Centroid, new Vector3(1f,0.1f,1f));
+        }
+            
+        //debug get cell
+        if (player) {
+            Cell cell = GridMap.GetCellFromWorldPosition(player.position);
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawCube(cell.Centroid, Vector3.one * 0.5f);
+        }
+    }
+}
---
+++ GridMeshGenerator/Assets/Scripts/Input/PlayerInputManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputManager : MonoBehaviour {
+    public GridGenerator Generator;
+    public CellOptions CellOptions;
+    
+    private Vector2 _cursorPosition;
+    
+    private PlayerControls _controls;
+    private Camera _camera;
+
+    private void Awake() {
+        _camera = Camera.main;
+        
+        _controls = new PlayerControls();
+        _controls.Preview.CursorPosition.performed += ctx => _cursorPosition = ctx.ReadValue<Vector2>();
+        _controls.Preview.Select.performed += OnSelect;
+    }
+
+    private void OnEnable() {
+        _controls.Preview.Enable();
+    }
+
+    private void OnDisable() {
+        _controls.Preview.Disable();
+    }
+
+    private void OnSelect(InputAction.CallbackContext obj) {
+        Debug.Log(_cursorPosition);
+        Ray ray = _camera.ScreenPointToRay(_cursorPosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit)) {
+            Debug.Log(hit.point);
+            CellOptions.CurrentCell = Generator.GridMap.GetCellFromWorldPosition(hit.point);
+        }
+    }
+}
